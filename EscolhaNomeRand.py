5bc936c6cb5f85b8db6d60b6bd01bf415064cacd
Samuel <samuelisaacandrademartins@gmail.com>
2026-05-17 12:29:18 -0300

    new file:   .vscode/settings.json 	modified:   EscolhaNomeRand.py 	new file:   TocaMP3.py 	new file:   teste.mp3

--- EscolhaNomeRand.py
+++ EscolhaNomeRand.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, shuffle
 
 a1 = str(input("Primeiro aluno: "))
 a2 = str(input("Segundo aluno: "))
@@ -8,3 +8,5 @@ a4 = str(input("Quarto aluno: "))
 lista = [a1,a2,a3,a4];
 
 print("O aluno escolhido foi: {}".format(choice(lista)))
+shuffle(lista)
+print("Agora, vou embaralhar e vai ficar assim: {}".format(lista))
